f386bc1b994f65a22381f80bd90c8d0513a8881e
當銘 大河 <t-toume@M-C02V307THH29.voyagegroup.local>
2019-06-25 17:06:09 +0900

    add api key

--- setting/coincheck_ccxt.py
+++ setting/coincheck_ccxt.py
@@ -7,10 +7,10 @@ import ccxt
 #### api keys #####
 api_keys = [
     {"api_key": "P80YN-QI1UVhVk76", "api_secret": "Th2T19fx7oe6yi45OpQesJSyR49CrKVQ"},
-    {"api_key": "P80YN-QI1UVhVk76", "api_secret": "Th2T19fx7oe6yi45OpQesJSyR49CrKVQ"},
-    {"api_key": "P80YN-QI1UVhVk76", "api_secret": "Th2T19fx7oe6yi45OpQesJSyR49CrKVQ"},
-    {"api_key": "P80YN-QI1UVhVk76", "api_secret": "Th2T19fx7oe6yi45OpQesJSyR49CrKVQ"},
-    {"api_key": "P80YN-QI1UVhVk76", "api_secret": "Th2T19fx7oe6yi45OpQesJSyR49CrKVQ"}
+    {"api_key": "4hyuNHWFuH66vDGY", "api_secret": "qahvbs54s-_49ulTclE7Y77NKHNPwiKa"},
+    {"api_key": "8lsM6sY0fcU1ExFg", "api_secret": "BBULQSREkwQldi4YmCpCepzrSeQWCoxp"},
+    {"api_key": "0bPwCLosj68KSL9d", "api_secret": "Q7K5xhre5Mrq5h70khFBWuIVjhJ_YbbD"},
+    {"api_key": "IvFhWpIWtO0rLDtq", "api_secret": "yNHXu3BEepJfD50nn2z9E7NwkFO-nR0N"}
 ]
 
 num = random.randint(0,4)
